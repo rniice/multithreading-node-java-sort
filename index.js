7fa8ce691b34571bfabbb7c640705de19c5e95f9
Michael Crockett <michael.crockett@gmail.com>
2016-08-24 00:08:49 -0400

    final step merging sorted arrays done in single thread

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var mergeSortArray          = require('./src/node/MergeSort');            //load
 var mergeSortedArrays       = require('./src/node/MergeSortedArrays');    //load the javascript merge sorted array libraries
 
 //scripts for multithreading
-//var forkJavaScript          = require('./src/node/forkJavaScript');       //load method to run new forked nodeJS process
+//var forkJavaScript          = require('./src/node/forkJavaScript');     //load method to run new forked nodeJS process
 var fork                    = require('child_process').fork;              //load child process fork dependencies
-
-
+var fs                      = require('fs');                              //load filesystem to save results
 
 java.classpath.push("bin/MergeSortBinaries.jar");                         //load the java merge sort libraries
 
@@ -19,11 +18,7 @@ var min_array_length  = 10;
 var max_array_length  = 100000;
 
 //var elapsed_ms_single_js    = testJavaScriptSingleThread(100000);
-var elapsed_ms_4x_js      = testJavaScriptMultipleThread(100000);
-
-
-
-
+var elapsed_ms_4x_js          = testJavaScriptMultipleThread(100000);
 
 
 
@@ -53,9 +48,11 @@ function testJavaScriptMultipleThread(length){
     ,test_array.slice(3*length_ea)
   ];
 
-  var fork_instance = [];
-  var fork_result = [];
-
+  var fork_instance         = [];
+  var fork_result           = [];
+  var forks_complete        = 0;
+  var elapsed_merge_sorted  = null;
+  //var forks_start_time = new Date().getTime();
   for (var i=0; i<array_data.length; i++){
     fork_instance.push(fork(cwd + '/src/node/ForkJavaScript.js',[],{silent: false}));
 
@@ -65,6 +62,28 @@ function testJavaScriptMultipleThread(length){
           fork_result.push(data);
           //console.log("fork results sorted: " + data.result);
           console.log("fork sort elapsed_ms: " + data.elapsed_ms);
+          //if other all 4 fors are completed start the merging
+          if(++forks_complete == 4){
+            forks_finish_time = new Date().getTime();
+            //var forks_complete_time = forks_finish_time - forks_start_time;
+            //console.log("forks_complete_time_ms: " + forks_complete_time);
+            console.log("beginning merge");
+
+            //do final sort on a single thread for now
+            var time_start            = new Date().getTime();
+            var sorted1               = mergeSortedArrays(fork_result[0].result, fork_result[1].result);
+            var sorted2               = mergeSortedArrays(fork_result[2].result, fork_result[3].result);
+            var sorted                = mergeSortedArrays(sorted1, sorted2);
+            var time_finish           = new Date().getTime();
+            elapsed_merge_sorted      = time_finish - time_start;
+            console.log("merged sorted arrays elapsed_ms: " + elapsed_merge_sorted);
+            saveResult(sorted);
+
+            var total_time_ms = fork_result[0].elapsed_ms + elapsed_merge_sorted;
+            console.log("javascript 4x thread elapsed ms: " + total_time_ms);
+            return total_time_ms;
+
+          }
       }
     });
 
@@ -72,20 +91,7 @@ function testJavaScriptMultipleThread(length){
     fork_instance[i].send({run: true});
 
   }
-  //fork1_result.send("testing sending data");
 
-/*
-  //combine the results
-  var half1         = mergeSortedArrays(array1, array2);
-  var half2         = mergeSortedArrays(array3, array4);
-  var result        = mergeSortedArrays(half1, half2);
-
-  var time_finish   = new Date().getTime();
-  var elapsed_ms    = time_finish - time_start;
-  console.log("javascript 4 thread elapsed ms: " + elapsed_ms);
-  */
-
-  //return elapsed_ms;
 }
 
 
@@ -101,6 +107,14 @@ function testJavaMultipleThread(length){
 
 }
 
+
+function saveResult(data){
+  fs.writeFile('result.txt', data, (err) => {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
+
+}
 
 //test single thread javascript for various lengths n array
 //test multiple thread javascript for various lengths n array
